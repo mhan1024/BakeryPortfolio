04f6880a70afe38f4eea7a48c975d085d9d6650d
Michelle Han <mhan10242019@gmail.com>
2025-08-12 19:44:37 -0400

    updated images

--- bakery-portfolio-app/src/components/DisplayCase.js
+++ bakery-portfolio-app/src/components/DisplayCase.js
@@ -12,35 +12,35 @@ function DisplayCase({ popUpImg, TAG_BACK }) {
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/java_tag.png', 0) }
                 >    
-                    <img src={ process.env.PUBLIC_URL +  '/images/java_button.png' } alt='java tiramisu' className='lang-img'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/java_button.png` } alt='java tiramisu' className='lang-img'/>
                 </button>
 
                 <button 
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/python_tag.png', 1) }
                 >
-                        <img src={ process.env.PUBLIC_URL + '/images/python_button.png' } alt='python macarons' className='lang-img'/>
+                        <img src={ `${process.env.PUBLIC_URL}/images/python_button.png` } alt='python macarons' className='lang-img'/>
                 </button>
 
                 <button 
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/swift_tag.png', 2) }
                 >
-                    <img src={ process.env.PUBLIC_URL + '/images/swift_button.png' } alt='swift lemon tart' className='lang-img'id='swift-lemon'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/swift_button.png` } alt='swift lemon tart' className='lang-img'id='swift-lemon'/>
                 </button>
 
                 <button 
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/kotlin_tag.png', 3) }
                 >
-                    <img src={ process.env.PUBLIC_URL + '/images/kotlin_button.png' } alt='kotlin mille feuille' className='lang-img'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/kotlin_button.png`} alt='kotlin mille feuille' className='lang-img'/>
                 </button>
 
                 <button 
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/javascript_tag.png', 4) }
                 >
-                    <img src={ process.env.PUBLIC_URL + '/images/javascript_button.png' } alt='javascript cupcake' className='lang-img'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/javascript_button.png` } alt='javascript cupcake' className='lang-img'/>
                 </button>
             </div>
 
@@ -50,35 +50,35 @@ function DisplayCase({ popUpImg, TAG_BACK }) {
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/html_tag.png', 5) }
                 >
-                    <img src={ process.env.PUBLIC_URL + '/images/html_button.png' } alt='html cookies' className='lang-img'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/html_button.png` } alt='html cookies' className='lang-img'/>
                 </button>
 
                 <button 
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/css_tag.png', 6) }
                 >
-                    <img src={ process.env.PUBLIC_URL + '/images/css_button.png' } alt='css parfait' className='lang-img' id='css-parfait'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/css_button.png` } alt='css parfait' className='lang-img' id='css-parfait'/>
                 </button>
 
                 <button 
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/mysql_tag.png', 7) }
                 >
-                    <img src={ process.env.PUBLIC_URL + '/images/mysql_button.png' } alt='mysql cheesecake' className='lang-img' id='mysql-cheesecake'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/mysql_button.png` } alt='mysql cheesecake' className='lang-img' id='mysql-cheesecake'/>
                 </button>
 
                 <button 
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/php_tag.png', 8) }
                 >
-                    <img src={ process.env.PUBLIC_URL + '/images/php_button.png' } alt='php banana bread' className='lang-img' id='php-banana'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/php_button.png` } alt='php banana bread' className='lang-img' id='php-banana'/>
                 </button>
 
                 <button 
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/c_sharp_tag.png', 9) }
                 >
-                    <img src={ process.env.PUBLIC_URL + '/images/c_sharp_button.png' } alt='c sharp black forrest cake' className='lang-img'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/c_sharp_button.png` } alt='c sharp black forrest cake' className='lang-img'/>
                 </button>
 
             </div>
@@ -88,14 +88,14 @@ function DisplayCase({ popUpImg, TAG_BACK }) {
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/c_tag.png', 10) }
                 >
-                    <img src={ process.env.PUBLIC_URL + '/images/c_button.png' } alt='c scones' className='lang-img'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/c_button.png` } alt='c scones' className='lang-img'/>
                 </button>
     
                 <button 
                     className='lang-buttons'
                     onClick={ () => popUpImg('/images/c_plus_plus_tag.png', 11) }
                 >
-                    <img src={ process.env.PUBLIC_URL + '/images/c_plus_plus_button.png' } alt='c plus plus flan' className='lang-img' id='c-plus-plus-flan'/>
+                    <img src={ `${process.env.PUBLIC_URL}/images/c_plus_plus_button.png` } alt='c plus plus flan' className='lang-img' id='c-plus-plus-flan'/>
                 </button>
             </div>
         </div>
